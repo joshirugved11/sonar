b8de554bc6dde67719e0d5615bd7a41bc84acfa2
Rugved Joshi <rugvedsamruddhi@gmail.com>
2025-09-18 20:09:01 +0530

    update: config file to fetch env secrets

--- agent/config.py
+++ agent/config.py
@@ -1,16 +1,9 @@
 from dotenv import load_dotenv
 import os
 
-load_dotenv()  # loads from .env file into os.environ
+load_dotenv()
 
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-ELEVENLABS_API_KEY = os.getenv("ELEVENLABS_API_KEY")
-
-USE_GEMINI = os.getenv("USE_GEMINI", "True") == "True"
-USE_ELEVENLABS_TTS = os.getenv("USE_ELEVENLABS_TTS", "False") == "True"
-USE_ELEVENLABS_STT = os.getenv("USE_ELEVENLABS_STT", "False") == "True"
-USE_OFFLINE_MODE = os.getenv("USE_OFFLINE_MODE", "True") == "True"
-
-
-STT_ENGINE = os.getenv("STT_ENGINE", "whisper")
-TTS_ENGINE = os.getenv("TTS_ENGINE", "pyttsx3")
+ELEVENLABS_API_KEY = os.getenv("ELEVENLABS_API_KEY", "")
+STT_ENGINE = os.getenv("STT_ENGINE", "google").lower()   # google or whisper
+TTS_ENGINE = os.getenv("TTS_ENGINE", "elevenlabs").lower()  # elevenlabs or pyttsx3
